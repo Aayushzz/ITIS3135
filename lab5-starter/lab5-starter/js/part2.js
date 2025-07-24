42a468696176a36fa74a26b3b1d63e8b25479871
Aayushzz <aayushnrla@gmail.com>
2025-07-24 18:15:40 -0400

    Completed Part 2

--- lab5-starter/lab5-starter/js/part2.js
+++ lab5-starter/lab5-starter/js/part2.js
@@ -43,6 +43,18 @@ const albumCollection = [
         title: 'Transformer',
         artist: 'Lou Reed',
         tracks: ['Vicious', 'Perfect Day', 'Walking on the Wild Side']
+    },
+    {
+        id: 4021,
+        title: 'Random Access Memories',
+        artist: 'Daft Punk',
+        tracks: ['Get Lucky', 'Instant Crush']
+    },
+    {
+        id: 4892,
+        title: 'The Dark Side of the Moon',
+        artist: 'Pink Floyd',
+        tracks: ['Time', 'Money']
     }
 
 ];
@@ -52,10 +64,10 @@ const albumCollection = [
  * @returns {array} - an array of titles of all the albums in albumCollection
  */
 function getAllTitles() {
-    
+    return albumCollection.map(album => album.title);
 }
 //uncomment following test code after implementing the function
-//console.log(getAllTitles());
+// console.log(getAllTitles());
 
 
 /**
@@ -64,7 +76,7 @@ function getAllTitles() {
  * @returns an array of albums of the specified artist in albumCollection
  */
 function getAlbumsByArtist(artist) {
-    
+    return albumCollection.filter(album => album.artist === artist)
 }
 //uncomment following test code after implementing the function
 // console.log(getAlbumsByArtist('Queen'));
@@ -77,7 +89,10 @@ function getAlbumsByArtist(artist) {
  * @returns an album object with the specified track
  */
 function getAlbumWithTrack(track) {
-    
+    return albumCollection.find(album => 
+        album.hasOwnProperty('tracks') && 
+        album.tracks.some(t => t.toLowerCase() === track.toLowerCase())
+    );
 }
 //uncomment following test code after implementing the function
 // console.log(getAlbumWithTrack('Little Red Corvette'));
@@ -91,18 +106,37 @@ function getAlbumWithTrack(track) {
  * @param {*} value - new value for the property
  */
 function updateAlbum(id, prop, value) {
+    const album = albumCollection.find(album => album.id === id);
 
+    if (!album) return;
+
+    if (value === '') {
+        delete album[prop];
+    } else {
+
+        if (prop === 'tracks') {
+        
+            if (!album.hasOwnProperty('tracks')) {
+                album.tracks = [];
+            }
+         
+            album.tracks.push(value);
+        } else {
+          
+            album[prop] = value;
+        }
+    }
 
 }
 //uncomment following test code after implementing the function
-// updateAlbum(3590, 'artist', 'Alice Cooper');
-// updateAlbum(3590, 'tracks', "School's Out");
-// updateAlbum(2125, 'artist', '');
-// updateAlbum(1678, 'tracks', 'Bohemian Rhapsody');
-// updateAlbum(2975, 'tracks', 'Free');
-// updateAlbum(1257, 'tracks', '');
-// updateAlbum(3257, 'artist', 'David Bowie');
-// console.log(albumCollection);
+updateAlbum(3590, 'artist', 'Alice Cooper');
+updateAlbum(3590, 'tracks', "School's Out");
+updateAlbum(2125, 'artist', '');
+updateAlbum(1678, 'tracks', 'Bohemian Rhapsody');
+updateAlbum(2975, 'tracks', 'Free');
+updateAlbum(1257, 'tracks', '');
+updateAlbum(3257, 'artist', 'David Bowie');
+console.log(albumCollection);
 
 
 
